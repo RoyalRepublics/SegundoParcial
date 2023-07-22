d013afd6b86f8f17d7abebe74a165fed028745f0
RoyalRepublics <132688377+RoyalRepublics@users.noreply.github.com>
2023-07-21 20:27:39 -0500

    Correccion Evaluacion1

---
+++ Poo_P2_Evaluacion_Animales/src/poo_p2_evaluacion_animales/Poo_P2_Evaluacion_Animales.java
@@ -0,0 +1,14 @@
+
+package poo_p2_evaluacion_animales;
+
+public class Poo_P2_Evaluacion_Animales {
+
+
+    public static void main(String[] args) {
+Vaca v1=new Vaca("Holstein","Blanco y Negro","AV","Bovino","Potrero","Activo en el dia","Herbiboro",4);
+Murcielago m1=new Murcielago("Nocturna", "Cuevas","Frutero","Murcielago","Montañas","Noctujrno","Frutas",2);
+v1.mostrarDatos();
+m1.mostrarDatos();
+    }
+    
+}
